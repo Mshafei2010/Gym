f738f53d46fbc9fd298662658ffb288a6356f190
Mshafei2010 <mshafei2010@gmail.com>
2023-04-13 21:29:50 +0200

    home & navbars

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProgramsComponent } from './programs/programs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home',component:HomePageComponent},
+  {path:'programs',component:ProgramsComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavbarComponent,
+    BottomBarComponent,
+    ProgramsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
